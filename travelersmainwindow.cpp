99f02410a98edcde7f27ca96331c2d34a5a41807
unknown <erichvickers@gmail.com>
2018-09-23 17:52:20 -0700

    new file:   CS_European_CIties.pro 	new file:   CS_European_CIties.pro.user 	new file:   CS_European_CIties.pro.user.bb98853 	new file:   dbmanager.cpp 	new file:   dbmanager.h 	new file:   main.cpp 	new file:   travelersmainwindow.cpp 	new file:   travelersmainwindow.h 	new file:   travelersmainwindow.ui

---
+++ travelersmainwindow.cpp
@@ -0,0 +1,17 @@
+#include "travelersmainwindow.h"
+#include "ui_travelersmainwindow.h"
+
+TravelersMainWindow::TravelersMainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::TravelersMainWindow)
+{
+	ui->setupUi(this);
+	DbManager controller;
+	controller.initDataBase();
+//    controller.readInTxtFile();
+}
+
+TravelersMainWindow::~TravelersMainWindow()
+{
+	delete ui;
+}
---
+++ travelersmainwindow.h
@@ -0,0 +1,24 @@
+#ifndef TRAVELERSMAINWINDOW_H
+#define TRAVELERSMAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "dbmanager.h"
+
+namespace Ui {
+class TravelersMainWindow;
+}
+
+class TravelersMainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit TravelersMainWindow(QWidget *parent = nullptr);
+	~TravelersMainWindow();
+
+private:
+	Ui::TravelersMainWindow *ui;
+};
+
+#endif // TRAVELERSMAINWINDOW_H
